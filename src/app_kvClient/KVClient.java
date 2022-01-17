2a44c6b9fed830c2c030b739f755cb996afe639d
kierancyphus <kierancyphus@gmail.com>
2022-01-17 15:32:52 -0500

    random stuff

--- src/app_kvClient/KVClient.java
+++ src/app_kvClient/KVClient.java
@@ -4,12 +4,12 @@ import client.KVCommInterface;
 
 public class KVClient implements IKVClient {
     @Override
-    public void newConnection(String hostname, int port) throws Exception{
+    public void newConnection(String hostname, int port) throws Exception {
         // TODO Auto-generated method stub
     }
 
     @Override
-    public KVCommInterface getStore(){
+    public KVCommInterface getStore() {
         // TODO Auto-generated method stub
         return null;
     }
