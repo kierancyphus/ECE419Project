fe8b3de07a44878c77476def536bccc9c061b85e
kierancyphus <kierancyphus@gmail.com>
2022-01-17 14:05:20 -0500

    created message instantiation

--- src/shared/messages/KVMessage.java
+++ src/shared/messages/KVMessage.java
@@ -1,37 +1,28 @@
 package shared.messages;
 
-public interface KVMessage {
-	
-	public enum StatusType {
-		GET, 			/* Get - request */
-		GET_ERROR, 		/* requested tuple (i.e. value) not found */
-		GET_SUCCESS, 	/* requested tuple (i.e. value) found */
-		PUT, 			/* Put - request */
-		PUT_SUCCESS, 	/* Put - request successful, tuple inserted */
-		PUT_UPDATE, 	/* Put - request successful, i.e. value updated */
-		PUT_ERROR, 		/* Put - request not successful */
-		DELETE_SUCCESS, /* Delete - request successful */
-		DELETE_ERROR 	/* Delete - request successful */
-	}
+public class KVMessage implements IKVMessage {
+    String key;
+    String value;
+    IKVMessage.StatusType status;
 
-	/**
-	 * @return the key that is associated with this message, 
-	 * 		null if not key is associated.
-	 */
-	public String getKey();
-	
-	/**
-	 * @return the value that is associated with this message, 
-	 * 		null if not value is associated.
-	 */
-	public String getValue();
-	
-	/**
-	 * @return a status string that is used to identify request types, 
-	 * response types and error types associated to the message.
-	 */
-	public StatusType getStatus();
-	
+    public KVMessage(String key, String value, IKVMessage.StatusType status) {
+        this.key = key;
+        this.value = value;
+        this.status = status;
+    }
+
+    @Override
+    public String getKey() {
+        return this.key;
+    }
+
+    @Override
+    public String getValue() {
+        return this.value;
+    }
+
+    @Override
+    public StatusType getStatus() {
+        return this.status;
+    }
 }
-
-
