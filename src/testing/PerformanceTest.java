fb77301f2ef095e42dc89648506e127ba0a6f914
Rui Gao <ruigao75@gmail.com>
2022-01-28 13:30:18 -0500

    changed test to client side, socket connection error

--- src/testing/PerformanceTest.java
+++ src/testing/PerformanceTest.java
@@ -2,67 +2,122 @@ package testing;
 
 import junit.framework.TestCase;
 import app_kvServer.KVServer;
+import client.KVStore;
 
+import shared.messages.IKVMessage;
+
+import java.io.IOException;
 import java.util.Random;
 
 public class PerformanceTest extends TestCase {
     private static final double NANO_MILLI = 1e6;
+    private static final double NANO_SEC = 1e9;
     private static final Random rand = new Random();
 
     public void test8020() {
+        KVStore kvClient = new KVStore("localhost", 50000);
         Exception ex = null;
         try {
-            runTime(0.80, 6000, 100);
+            kvClient.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            runTime(kvClient,0.80, 10000, 200);
         } catch (Exception e) {
             ex = e;
         }
         assertNull(ex);
     }
+//
+//    public void test6535() {
+//        KVStore kvClient = new KVStore("localhost", 50000);
+//        Exception ex = null;
+//        try {
+//            kvClient.connect();
+//        } catch (Exception e) {
+//            ex = e;
+//        }
+//        try {
+//            runTime(kvClient,0.65, 10000, 200);
+//        } catch (Exception e) {
+//            ex = e;
+//        }
+//        assertNull(ex);
+//    }
+//
+//    public void test5050() {
+//        KVStore kvClient = new KVStore("localhost", 50000);
+//        Exception ex = null;
+//        try {
+//            kvClient.connect();
+//        } catch (Exception e) {
+//            ex = e;
+//        }
+//        try {
+//            runTime(kvClient,0.50, 10000, 200);
+//        } catch (Exception e) {
+//            ex = e;
+//        }
+//        assertNull(ex);
+//    }
+//
+//    public void test3565() {
+//        KVStore kvClient = new KVStore("localhost", 50000);
+//        Exception ex = null;
+//        try {
+//            kvClient.connect();
+//        } catch (Exception e) {
+//            ex = e;
+//        }
+//        try {
+//            runTime(kvClient,0.35, 10000, 200);
+//        } catch (Exception e) {
+//            ex = e;
+//        }
+//        assertNull(ex);
+//    }
+//
+//    public void test2080() {
+//        KVStore kvClient = new KVStore("localhost", 50000);
+//        Exception ex = null;
+//        try {
+//            kvClient.connect();
+//        } catch (Exception e) {
+//            ex = e;
+//        }try {
+//            runTime(kvClient,0.20, 10000, 200);
+//        } catch (Exception e) {
+//            ex = e;
+//        }
+//        assertNull(ex);
+//    }
 
-    public void test5050() {
-        Exception ex = null;
+    public void runTime(KVStore client, double percentPuts, int numRequests, int numUniqueKeys) throws Exception{
+        // Currently, caching isn't implemented, so the arguments are useless
+        int putCounter = 0;
+        int getCounter = 0;
+        long reqStart, reqEnd;
+        boolean[] putOrGet = new boolean[numRequests];
+        long[] reqTime = new long[numRequests];
         try {
-            runTime(0.50, 6000, 100);
-        } catch (Exception e) {
-            ex = e;
-        }
-        assertNull(ex);
-    }
-
-    public void test2080() {
-        Exception ex = null;
-        try {
-            runTime(0.20, 6000, 100);
-        } catch (Exception e) {
-            ex = e;
-        }
-        assertNull(ex);
-    }
-
-    public void runTime(double percentPuts, int numRequests, int numUniqueKeys) {
-        try {
-            // Currently, caching isn't implemented, so the arguments are useless
-            KVServer server = new KVServer(33333, 1, "FIFO");
-            int putCounter = 0;
-            int getCounter = 0;
-            long reqStart, reqEnd;
-            boolean[] putOrGet = new boolean[numRequests];
-            long[] reqTime = new long[numRequests];
-
             for (int i = 0; i < numRequests; i++) {
                 boolean doPut = rand.nextDouble() < percentPuts;
                 String rand_key = "key" + rand.nextInt(numUniqueKeys);
+                String rand_value = "key" + rand.nextInt(numRequests);
                 reqStart = System.nanoTime();
                 if (doPut) {
                     try {
-                        server.putKV(rand_key, "test_value");
+                        client.put(rand_key, rand_value);
                     } catch (Exception e) {
+                        throw e;
                     }
                     putOrGet[i] = true;
                 } else {
                     try {
-                        server.getKV(rand_key);
+                        client.get(rand_key);
                     } catch (Exception e) {
+                        throw e;
                     }
                     putOrGet[i] = false;
                 }
@@ -89,16 +144,18 @@ public class PerformanceTest extends TestCase {
             long totalTime = getTime + putTime;
 
             long totalAverage = totalTime / numRequests;
-            long getAverage = getTime / getCounter;
             long putAverage = putTime / putCounter;
+            long getAverage = getTime / getCounter;
+            long throughput = (long) (numRequests / (totalTime / NANO_SEC));
+            System.out.println("Put Percentage: " + percentPuts);
+            System.out.println("Average Throughput: " + throughput);
             System.out.println("Total time average: " + totalAverage / NANO_MILLI);
-            System.out.println("Get time average: " + getAverage / NANO_MILLI);
-            System.out.println("\tGet time min: " + getMin / NANO_MILLI);
-            System.out.println("\tGet time max: " + getMax / NANO_MILLI);
             System.out.println("Put time average: " + putAverage / NANO_MILLI);
             System.out.println("\tPut time min: " + putMin / NANO_MILLI);
             System.out.println("\tPut time max: " + putMax / NANO_MILLI);
-
+            System.out.println("Get time average: " + getAverage / NANO_MILLI);
+            System.out.println("\tGet time min: " + getMin / NANO_MILLI);
+            System.out.println("\tGet time max: " + getMax / NANO_MILLI);
         } catch (Exception e) {
             e.printStackTrace();
         }
