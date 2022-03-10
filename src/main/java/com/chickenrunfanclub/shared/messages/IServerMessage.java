512a9de5f1cf079d0c67cfff7e20b86acd590234
Johnny Ye <jye@Johnnys-MacBook-Air.local>
2022-03-10 16:20:56 -0500

    Added message typed for server messages

---
+++ src/main/java/com/chickenrunfanclub/shared/messages/IServerMessage.java
@@ -0,0 +1,35 @@
+package com.chickenrunfanclub.shared.messages;
+
+public interface IServerMessage {
+
+    public enum StatusType {
+        SERVER_START,				/* Start Server, all requests are processed */
+        SERVER_STOP,				/* Stop server, no requests are processed */
+        SERVER_WRITE_UNLOCKED,		/* Unlock Write, write requests are processed */
+        SERVER_MOVE_DATA,			/* Move data between servers based on host and port */
+        SERVER_UPDATE_METADATA		/* Server metadata is updated */
+    }
+
+    /**
+     * @return the key that is associated with this message,
+     * 		null if not key is associated.
+     */
+    public String getKey();
+
+    /**
+     * @return the value that is associated with this message,
+     * 		null if not value is associated.
+     */
+    public String getValue();
+
+    /**
+     * @return a status string that is used to identify request types,
+     * response types and error types associated to the message.
+     */
+    public IServerMessage.StatusType getStatus();
+
+    /**
+     * @return a string in the form `status key value` to be used in message passing
+     * */
+    public String toString();
+}
